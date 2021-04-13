407ba91a5b1137921cd05ca1f23d981d489a7714
Mobina <shbmobina@gmail.com>
2021-04-13 22:03:56 +0430

    Add api documentation for dtos

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,9 +1,9 @@
-import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { ApiProperty } from '@nestjs/swagger';
 
 export default class CreateUserDto {
     @ApiProperty({type: "string", description:"user name", maxLength: 500})
     readonly name: string;
 
-    @ApiPropertyOptional({type: "array", items: {type: "number"}, description:"the book ids associated with the user"})
+    @ApiProperty({type: "array", items: {type: "number"}, description:"the book ids associated with the user"})
     readonly books: number[] ;
 }
