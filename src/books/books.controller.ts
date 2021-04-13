4bfeba6f27599b7ef13b3b61ef187c116b597287
Mobina <shbmobina@gmail.com>
2021-04-14 00:40:18 +0430

    Add update and delete functions for books

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Put, Delete, Param } from '@nestjs/common';
 import {ApiResponse} from '@nestjs/swagger';
 import { BooksServices } from './books.service';
 import CreateBookDto from './dto/create-book.dto';
+import UpdateBookDto from './dto/update-book.dto';
 
 
 @Controller('books')
@@ -10,13 +11,25 @@ export class BooksController {
 
     @ApiResponse({ status: 200, description: "handles creation of new books" })
     @Post('post')
-    postBook( @Body() genre: CreateBookDto) {
-        return this.booksServices.insert(genre);
+    postBook( @Body() book: CreateBookDto) {
+        return this.booksServices.insert(book);
     }
 
     @ApiResponse({ status: 200, description: "returns a list of existing books in the database" })
     @Get()
     getAll() {
         return this.booksServices.getAllBooks();
+    }
+
+    @ApiResponse({ status: 200, description: "updates attributes of an existing book" })
+    @Put('update')
+    updateBook( @Body() bookDetails: UpdateBookDto) {
+        return this.booksServices.update(bookDetails);
+    }
+
+    @ApiResponse({ status: 200, description: "deletes the book having 'bookId'" })
+    @Delete('delete')
+    deleteBook( @Param() bookId: number) {
+        return this.booksServices.delete(bookId);
     }
 }
