467c123dea66ae17a89243ed5351fcf2bb533197
Mobina <shbmobina@gmail.com>
2021-04-15 17:53:37 +0430

    Add insertFreelancer and insertSkill

--- src/jobseekers/jobseekers.service.ts
+++ src/jobseekers/jobseekers.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import EmployerEntity from 'src/db/entity/employer.entity';
+import FreelancerEntity from 'src/db/entity/freelancer.entity';
 import OrderEntity from 'src/db/entity/order.entity';
+import SkillEntity from 'src/db/entity/skill.entity';
 import CreateEmployerDto from './dto/create-employer.dto';
+import CreateFreelancerDto from './dto/create-freelancer.dto';
 import CreateOrderDto from './dto/create-order.dto';
+import CreateSkillDto from './dto/create-skill.dto';
 
 @Injectable()
 export class JobseekersService {
@@ -29,5 +33,25 @@ export class JobseekersService {
     newOrder.minWorkExp = minWorkExp;
     await OrderEntity.save(newOrder);
     return newOrder;
+  }
+  async insertFreelancer(freelancerDetails: CreateFreelancerDto): Promise<FreelancerEntity> {
+    const freelancerEntity: FreelancerEntity = FreelancerEntity.create();
+    const {id, name, cardNum} = freelancerDetails;
+    freelancerEntity.id = id;
+    freelancerEntity.name = name;
+    freelancerEntity.cardNum = cardNum;
+    freelancerEntity.skills = [];
+    await FreelancerEntity.save(freelancerEntity);
+    return freelancerEntity;
+  }
+  async insertSkill(skillDetails: CreateSkillDto, freelancerId: number): Promise<SkillEntity> {
+    const newSkill = SkillEntity.create();
+    const {skillId, name, level} = skillDetails;
+    newSkill.id = skillId;
+    newSkill.freelancerId = await FreelancerEntity.findOne(freelancerId);
+    newSkill.name = name;
+    newSkill.level = level;
+    await SkillEntity.save(newSkill);
+    return newSkill;
   }
 }
